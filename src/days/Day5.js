813d13f1be2a1bfaafb44bcd39a0c31492c70a8e
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-04-07 17:51:55 -0300

    reparo day 5

--- src/days/Day5.js
+++ src/days/Day5.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import "./day5.css"
 
 
@@ -24,10 +24,6 @@ const Day5 = () => {
         }
     })
 
-    // tenerlo en string para quitar los dos primeros numeros
-    // pasar a number antes de poner
-
-    console.log("1".slice(0.2))
     return (
         <div className="days day5">
             <main className="day5__card">
@@ -43,51 +39,48 @@ const Day5 = () => {
                         <span className='day5__span'>Purchases</span>    
                     </div>
                     <div className='day5__center'>
-                        <div className='day5__bar-red'
-                        style={{clipPath: `polygon(4% ${data.views.mon + 1}%, 20% ${data.views.tue + 1}%, 36% ${data.views.wed + 1}%, 52% ${data.views.thu + 1}%, 67% ${data.views.fri + 1}%, 81% ${data.views.sat + 1}%, 96% ${data.views.sun + 1}%, 96% ${data.views.sun + 4}%, 81% ${data.views.sat + 4}%, 67% ${data.views.fri + 4}%, 52% ${data.views.thu + 4}%, 36% ${data.views.wed + 4}%, 20% ${data.views.tue + 4}%, 4% ${data.views.mon + 4}%)`}}
-                        ></div>
-                        <div className='day5__bar-blue' 
-                        style={{clipPath: `polygon(4% ${data.purchases.mon + 1}%, 20% ${data.purchases.tue + 1}%, 36% ${data.purchases.wed + 1}%, 52% ${data.purchases.thu + 1}%, 67% ${data.purchases.fri + 1}%, 81% ${data.purchases.sat + 1}%, 96% ${data.purchases.sun + 1}%, 96% ${data.purchases.sun + 4}%, 81% ${data.purchases.sat + 4}%, 67% ${data.purchases.fri + 4}%, 52% ${data.purchases.thu + 4}%, 36% ${data.purchases.wed + 4}%, 20% ${data.purchases.tue + 4}%, 4% ${data.purchases.mon + 4}%)`}}></div>
-                        <div className='day5__circle-red day5__mon' style={{left: "3%",bottom:`${data.views.mon}%`}}>
+                        <div className='day5__bar-red'></div>
+                        <div className='day5__bar-blue' ></div>
+                        <div className='day5__circle-red day5__mon'>
                             <span className='day5__number-red'>{data.views.mon}</span>
                         </div>
-                        <div className='day5__circle-red day5__tue' style={{left: "19%",bottom:`${data.views.tue}%`}}>
+                        <div className='day5__circle-red day5__tue'>
                            <span className='day5__number-red'>{data.views.tue}</span>
                         </div>
-                        <div className='day5__circle-red day5__wed' style={{left: "35%",bottom:`${data.views.wed}%`}}>
+                        <div className='day5__circle-red day5__wed'>
                             <span className='day5__number-red'>{data.views.wed}</span>
                         </div>
-                        <div className='day5__circle-red day5__thu' style={{left: "51%",bottom:`${data.views.thu}%`}}>
+                        <div className='day5__circle-red day5__thu'>
                             <span className='day5__number-red'>{data.views.thu}</span>
                         </div>
-                        <div className='day5__circle-red day5__fri' style={{left: "66%",bottom:`${data.views.fri}%`}}>
+                        <div className='day5__circle-red day5__fri'>
                             <span className='day5__number-red'>{data.views.dri}</span>
                         </div>
-                        <div className='day5__circle-red day5__sat' style={{left: "80%",bottom:`${data.views.sat}%`}}>
+                        <div className='day5__circle-red day5__sat'>
                             <span className='day5__number-red'>{data.views.sat}</span>
                         </div>
-                        <div className='day5__circle-red day5__sun' style={{left: "95%",bottom:`${data.views.sun}%`}}>
+                        <div className='day5__circle-red day5__sun'>
                             <span className='day5__number-red'>{data.views.sun}</span>
                         </div>
-                        <div className='day5__circle-blue day5__mon' style={{left: "3%",bottom:`${data.purchases.mon}%`}}>
+                        <div className='day5__circle-blue day5__mon'>
                             <span className='day5__number-blue'>{data.purchases.mon}</span>
                         </div>
-                        <div className='day5__circle-blue day5__tue' style={{left: "19%",bottom:`${data.purchases.tue}%`}}>
+                        <div className='day5__circle-blue day5__tue'>
                             <span className='day5__number-blue'>{data.purchases.tue}</span>
                         </div>
-                        <div className='day5__circle-blue day5__wed' style={{left: "35%",bottom:`${data.purchases.wed}%`}}>
+                        <div className='day5__circle-blue day5__wed'>
                             <span className='day5__number-blue'>{data.purchases.wed}</span>
                         </div>
-                        <div className='day5__circle-blue day5__thu' style={{left: "51%",bottom:`${data.purchases.thu}%`}}>
+                        <div className='day5__circle-blue day5__thu'>
                             <span className='day5__number-blue'>{data.purchases.thu}</span>
                         </div>
-                        <div className='day5__circle-blue day5__fri' style={{left: "66%",bottom:`${data.purchases.fri}%`}}>
+                        <div className='day5__circle-blue day5__fri'>
                             <span className='day5__number-blue'>{data.purchases.fri}</span>
                         </div>
-                        <div className='day5__circle-blue day5__sat' style={{left: "80%",bottom:`${data.purchases.sat}%`}}>
+                        <div className='day5__circle-blue day5__sat'>
                             <span className='day5__number-blue'>{data.purchases.sat}</span>
                         </div>
-                        <div className='day5__circle-blue day5__sun' style={{left: "95%",bottom:`${data.purchases.sun}%`}}>
+                        <div className='day5__circle-blue day5__sun'>
                             <span className='day5__number-blue'>{data.purchases.sun}</span>
                         </div>
                     </div>
