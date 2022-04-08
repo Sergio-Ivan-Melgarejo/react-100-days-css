eaf834b16c9363acbf8a85f98a6874a68fd408ed
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-04-08 17:01:14 -0300

    error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div className='errorPage'>
-        <h1>Error Page</h1>
+    <div className='errorPage text-center py-5'>
+        <h1 className='pt-5'>Error 404</h1>
+        <p className='text py-3'>Page not found</p>
+        <Link to={"/"} className="btn-outline-primary p-2 px-5 rounded " >Back to Home</Link>
     </div>
   )
 }
