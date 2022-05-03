6d3cd1023f65725cc9388c66cfb74cf303e6b4d7
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-05-03 17:49:17 -0300

    day48

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import LanguageContext from '../Context/LanguageContext'
 
@@ -49,6 +49,7 @@ import Day44 from '../days/Day44'
 import Day45 from '../days/Day45'
 import Day46 from '../days/Day46'
 import Day47 from '../days/Day47'
+import Day48 from '../days/Day48'
 
 const Card = () => {  
   // Select Day
@@ -157,7 +158,9 @@ const Card = () => {
         return <Day46/>
         case 47:
         return <Day47 />
-        default: return <Day47 />
+        case 48:
+        return <Day48 />
+        default: return <Day48 />
       }
     }
     return <Day1
@@ -183,11 +186,11 @@ const Card = () => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7"></path></svg>
         </Link>
         </div>
-        <div onClick={hancleResicle} className='container-button'>
+        <div className='container-button'>
           <a href={daysData[dayNum - 1].codepen} target="_blank" rel="noreferrer" className='codePen'>
             <svg viewBox="0 0 100 100" fill="currentColor"><path d="M100 34.2c-.4-2.6-3.3-4-5.3-5.3-3.6-2.4-7.1-4.7-10.7-7.1-8.5-5.7-17.1-11.4-25.6-17.1-2-1.3-4-2.7-6-4-1.4-1-3.3-1-4.8 0-5.7 3.8-11.5 7.7-17.2 11.5L5.2 29C3 30.4.1 31.8 0 34.8c-.1 3.3 0 6.7 0 10v16c0 2.9-.6 6.3 2.1 8.1 6.4 4.4 12.9 8.6 19.4 12.9 8 5.3 16 10.7 24 16 2.2 1.5 4.4 3.1 7.1 1.3 2.3-1.5 4.5-3 6.8-4.5 8.9-5.9 17.8-11.9 26.7-17.8l9.9-6.6c.6-.4 1.3-.8 1.9-1.3 1.4-1 2-2.4 2-4.1V37.3c.1-1.1.2-2.1.1-3.1 0-.1 0 .2 0 0zM54.3 12.3L88 34.8 73 44.9 54.3 32.4V12.3zm-8.6 0v20L27.1 44.8 12 34.8l33.7-22.5zM8.6 42.8L19.3 50 8.6 57.2V42.8zm37.1 44.9L12 65.2l15-10.1 18.6 12.5v20.1zM50 60.2L34.8 50 50 39.8 65.2 50 50 60.2zm4.3 27.5v-20l18.6-12.5 15 10.1-33.6 22.4zm37.1-30.5L80.7 50l10.8-7.2-.1 14.4z"></path></svg>
           </a>
-          <button className='reset'>
+          <button onClick={hancleResicle} className='reset'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
           </button>
         </div>
---
+++ src/days/Day48.js
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./day48.scss"
+
+const Day48 = () => {
+  return (
+    <div className='days day48'>
+        <div className="day48__center">
+            <div className="day48__cube" onclick="void(0);">
+                <div className="day48__side day48__front">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+                <div className="day48__side day48__back">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+                <div className="day48__side day48__right">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+                <div className="day48__side day48__left">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+                <div className="day48__side day48__top">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+                <div className="day48__side day48__bottom">
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                    <div className='day48__box'></div>
+                </div>
+            </div>
+	    </div>
+    </div>
+  )
+}
+
+export default Day48
